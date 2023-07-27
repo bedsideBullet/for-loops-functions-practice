ada3434584b7e83e1b232195d801df9d51ed74af
Daniel Smith <danjsmith3@gmail.com>
2023-07-26 21:53:26 -0500

    finished e14

--- exercises/e13.js
+++ exercises/e13.js
@@ -1,4 +1,3 @@
-
 // EXERCISE 13
 // Return an array of bank accounts that have a sum of deposits less than 2000 or no deposits at all
 // Array example: bankAccounts in /data/data.js
@@ -6,10 +5,26 @@
 
 export function getAllAccountsWithSumsOfDepositsLess2000(array) {
   // Your code goes here...
+  const accountsWithSumsLess2000 = [];
 
+  for (let i = 0; i < array.length; i++) {
+    const account = array[i];
+    const deposits = account.deposits;
+
+    let sumOfDeposits = 0;
+    if (deposits) {
+      for (let j = 0; j < deposits.length; j++) {
+        sumOfDeposits += deposits[j];
+      }
+    }
+
+    if (!deposits || sumOfDeposits < 2000) {
+      accountsWithSumsLess2000.push(account);
+    }
+  }
+
+  return accountsWithSumsLess2000;
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-13"
--- exercises/e14.js
+++ exercises/e14.js
@@ -1,16 +1,38 @@
-
 // EXERCISE 14
 // The balance is supposed to equal the difference of all deposits and all withdrawals.
 // Check every bank account balance and return the array of bank accounts with a wrong balance
 // Array example: bankAccounts in /data/data.js
 // getClientsWithWrongBalance(bankAccounts) => [{ name: 'Name1', balance: 32, ... }, { name: 'Name2', balance: 3523, ... }]
 
+import { bankAccounts } from "../data/data";
+
 export function getClientsWithWrongBalance(array) {
-  // Your code goes here...
+  var wrongBalance = [];
 
+  for (let i = 0; i < array.length; i++) {
+    var bankAccount = array[i];
+    var totalDeposits = 0;
+    var totalWithdrawals = 0;
+
+    if (bankAccount.deposits) {
+      for (let j = 0; j < bankAccount.deposits.length; j++) {
+        totalDeposits += bankAccount.deposits[j];
+      }
+    }
+
+    if (bankAccount.withdrawals) {
+      for (let k = 0; k < bankAccount.withdrawals.length; k++) {
+        totalWithdrawals += bankAccount.withdrawals[k];
+      }
+    }
+
+    if (totalDeposits - totalWithdrawals !== bankAccount.balance) {
+      wrongBalance.push(bankAccount);
+    }
+  }
+
+  return wrongBalance;
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-14"
